8af92399ce34b8bcd51a8eef1bbbcdbdd5957385
wwz09 <47264059+wwz09@users.noreply.github.com>
2023-10-04 14:04:29 +0800

    ipq

--- package/kernel/mac80211/files/lib/wifi/mac80211.sh
+++ package/kernel/mac80211/files/lib/wifi/mac80211.sh
@@ -182,8 +182,9 @@ detect_mac80211() {
 			set wireless.default_radio${devidx}.device=radio${devidx}
 			set wireless.default_radio${devidx}.network=lan
 			set wireless.default_radio${devidx}.mode=ap
-			set wireless.default_radio${devidx}.ssid=OpenWrt
-			set wireless.default_radio${devidx}.encryption=none
+			set wireless.default_radio${devidx}.ssid=YM520
+			set wireless.default_radio${devidx}.encryption=psk2
+                        set wireless.default_radio${devidx}.key='abc5124937,'
 EOF
 		uci -q commit wireless
 
